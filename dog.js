bc969287c02fc5448b5ea79958ff7821007298f8
Anikroy7 <roy645800@gmail.com>
2022-02-27 17:09:04 +0600

    full dog opi done

--- dog.js
+++ dog.js
@@ -1,1 +1,28 @@
-console.log('cool')
+const loadDog = () => {
+    fetch('https://api.thedogapi.com/v1/breeds')
+        .then(res => res.json())
+        .then(data => displayDog(data));
+}
+
+const displayDog = (dogs) => {
+    // console.log(dogs)
+    const neededDogs = dogs.slice(0, 12);
+    const displayArea = document.getElementById('display-area');
+    // console.log(neededDogs)
+    for (const dog of neededDogs) {
+        const div = document.createElement('div');
+        div.classList.add('display-dog')
+        div.innerHTML = `
+                <div class="single-dog">
+                <img width="220px" src="${dog.image.url}" alt="">
+                <h3 class="text-danger">${dog.name}</h3>             
+                <p>${dog.temperament.slice(0, 50)}</p>
+                <p>${dog.weight.imperial}</p>
+                <div/>
+
+        `
+        displayArea.appendChild(div)
+        console.log(dog)
+
+    }
+}
